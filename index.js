7de498a76f1a557d19960c80113626f5cfa15647
Ganesh Jainarain <RicheyJay2@gmail.com>
2021-04-24 20:41:22 -0400

    created a user database schema in the User.js file, in the auth file we created the router and body parser going to enable body parser now in the index.js file

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
+
+dotenv.config();
+
+//Connect to DB
+mongoose.connect(
+    process.env.DB_CONNECT,
+    { useNewUrlParser: true },
+    () => console.log('Connected to DB!'));
 
 //Import routes
 const authRoute = require('./routes/auth');
+
+//Middlewares
+app.use(express)
+
 
 
 //Route Middlewares
