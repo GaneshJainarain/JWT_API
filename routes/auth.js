2c71e157afceb515923707d6b11401617ea558af
Ganesh Jainarain <RicheyJay2@gmail.com>
2021-04-25 17:45:09 -0400

    installed @hapi/joi for validation, added a validation.js file to handle data validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,17 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const {registerValidation, loginValidation} = require('../validation');
+
+
 
 
 router.post('/register', async (req, res) => {
+
+    //LETS VALIDATE THE DATA BEFORE WE MAKE A USER
+    const { error } = registerValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message)
+
+    //Create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
