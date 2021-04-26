8a6727f43964dfad3d97de2424e4b6a3155898f4
Ganesh Jainarain <RicheyJay2@gmail.com>
2021-04-26 01:59:37 -0400

    installed bcrypt and used it for hashing the passwords working on the demo presentations now

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # JWT_API
+
+# Demo1
+![](CreatingAUser.png)
+
+# Demo2
+![](EmailAlreadyExistsTest.png)
+
+
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const bcrypt = require('bcryptjs');
 const {registerValidation, loginValidation} = require('../validation');
 
 
@@ -16,15 +17,21 @@ router.post('/register', async (req, res) => {
     const emailExists = await User.findOne({ email: req.body.email});
     if (emailExists) return res.status(400).send('Email already exists');
 
+    //Hash Passwords
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+
     //Create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
         
     });
     try{
         const savedUser = await user.save();
+        res.send({ user: user._id})
         res.send(savedUser);
     } catch(err){
         res.status(400).send(err);
